712d71777e092dc4aa701185ed11b6cd9601b3ea
EliasAlmlof <elias.almlof@umain.com>
2026-05-04 10:35:46 +0200

    MySQL migration Query

--- backend/src/main/java/com/kth/snomos/backend/Repository/UserRepo.java
+++ backend/src/main/java/com/kth/snomos/backend/Repository/UserRepo.java
@@ -16,21 +16,21 @@ public interface UserRepo extends JpaRepository<User, Long> {
     @Query(value = "SELECT * FROM festival_user where username = :val1 AND password = :val2", nativeQuery = true)
     User rightPassword(@Param("val1") String name, @Param("val2") String password);
 
-    @Query(value = "SELECT EXISTS(SELECT 1 FROM festival_user WHERE username = :val)" , nativeQuery = true)
+    @Query(value = "SELECT COUNT(*) > 0 FROM festival_user WHERE username = :val" , nativeQuery = true)
     boolean userExists(@Param("val") String username);
 
     @Query(value = "SELECT * FROM festival f JOIN booking b ON f.festival_id = b.festivalid " +
-            "WHERE b.userid = :userid ORDER BY festival_date, festival_name", nativeQuery = true)
+            "WHERE b.userid = :userid ORDER BY f.festival_date, f.festival_name", nativeQuery = true)
     List<Festival> findBookingsByUser (@Param("userid") long userid);
 
-    @Query(value = "SELECT email FROM festival_user WHERE userid = :val", nativeQuery = true)
+    @Query(value = "SELECT email FROM festival_user WHERE user_id = :val", nativeQuery = true)
     String getEmail(@Param("val") int userid);
 
     @Modifying
-    @Query(value = "UPDATE festival_user SET email = :val1 WHERE userid = :val2",nativeQuery = true)
+    @Query(value = "UPDATE festival_user SET email = :val1 WHERE user_id = :val2",nativeQuery = true)
     void updateEmail(@Param("val1") String email, @Param("val2") int id);
 
     @Modifying
-    @Query(value = "DELETE FROM festival_user WHERE userid = :val", nativeQuery = true)
+    @Query(value = "DELETE FROM festival_user WHERE user_id = :val", nativeQuery = true)
     void deleteUser(@Param("val") int id);
 }
